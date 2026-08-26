b79a3505a1f7caa459af3fb65c63a55f6cc8be90
priyanshu thakur <priyqnshu94@gmail.com>
2026-08-26 19:01:26 +0530

    problem no.1661 solved

---
+++ 1661.sql
@@ -0,0 +1,6 @@
+SELECT A1.machine_id ,ROUND(AVG(A2.timestamp-A1.timestamp),3)as processing_time
+FROM Activity as A1 join Activity as A2
+on A1.machine_id = A2.machine_id AND A1.process_id=A2.process_id
+AND A1.activity_type="start"
+AND A2.activity_type="end"
+GROUP BY A1.machine_id;
