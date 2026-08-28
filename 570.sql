e2bd2df3eb796a5cccda225025b5505156cae67d
priyanshu thakur <priyqnshu94@gmail.com>
2026-08-28 17:27:37 +0530

    readme change

--- 570.sql
+++ 570.sql
@@ -3,3 +3,5 @@ Employee as e1  join Employee as e2
 on e1.id=e2.managerId
 group by e1.id,e1.name
 having count(e2.id)>=5;
+
+-- The HAVING clause is used in SQL to filter the results of a GROUP BY query based on aggregate functions. 
