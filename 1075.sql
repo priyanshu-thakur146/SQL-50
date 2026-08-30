c6d5cc942de2769bb154ec2bab4156e07a37ad0f
priyanshu thakur <priyqnshu94@gmail.com>
2026-08-30 12:34:19 +0530

    problem no.1075 solved

---
+++ 1075.sql
@@ -0,0 +1,5 @@
+
+select distinct p.project_id,round(sum(e.experience_years)/count(p.employee_id),2) as average_years
+from Project p inner join Employee e
+on p.employee_id=e.employee_id
+group by p.project_id;
